042be0a11ac11c7e8275589a2bad2412c1acbd3e
Sebastian Rust <sebastian@cryptolab.net>
2014-05-18 01:32:49 +0200

    implemented: syscall, fix: hostnamectl, todo: param, codecleanup

--- kernel-module/.sarlkm.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/osp/ospsshfs/sarlkm.o := ld -m elf_i386   -r -o /home/osp/ospsshfs/sarlkm.o /home/osp/ospsshfs/sar.o 
--- kernel-module/.tmp_versions/sarlkm.mod
+++
@@ -1,2 +0,0 @@
-/home/osp/ospsshfs/sarlkm.ko
-/home/osp/ospsshfs/sar.o
